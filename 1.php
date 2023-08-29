e5e87d13db76038251b1f414c53fa3133a90827f
muustapha <moustaf6920@hotmail.fr>
2023-08-30 01:25:45 +0200

    maison

--- 1.php
+++ 1.php
@@ -4,11 +4,16 @@ $tab=array(
 array('x'),
 array('y')
 );
+//affichage tableau2d 
+echo $tab[0][0];
+echo $tab[0][1]
+
+for(ligne=0; ligne<2; ligne++){
+    echo"<ul>";
+    for($colonne=0;$colone<2;$colone++ ){
+        echo "<li> .$tab[$ligne][$colone]. </li>"
+    }
+echo'<:ul>'
+}
 
 
-
-
-
-
-
-// vari
